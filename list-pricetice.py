4e227386e3bfed7a902dffe612afadc59572722b
Patt Sookmark <pijiya@sookmark.com>
2023-08-31 14:39:21 -0600

    Add practice

--- list-pricetice.py
+++ list-pricetice.py
@@ -1,9 +1,20 @@
 states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
 
-print(states_of_america)
-
 
 dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
+
+first_item = states_of_america[0]
+print('1st', first_item)
+
+count_items = len(states_of_america)
+print("count items", count_items)
+
+range = states_of_america[0:2]
+print(range)
+
+if "Colorado" in states_of_america:
+    print("Yes, Colorado is a state in America")
 
 #method
 # append()
