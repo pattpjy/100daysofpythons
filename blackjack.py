4aee6ca5e17bf068a52366bb5da5e6f589fb2a15
Patt Sookmark <pijiya@sookmark.com>
2023-10-03 09:35:20 -0600

    Add players card method

--- blackjack.py
+++ blackjack.py
@@ -43,20 +43,6 @@ def deal_card():
     return card
 
 
-# Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-user_cards = []
-computer_cards = []
-
-for _ in range(2):
-    new_card = deal_card()
-    user_cards.append(new_card)
-    computer_cards.append(deal_card())
-
-# Hint 6: Create a function called calculate_score() that takes a List of cards as input
-# and returns the score.
-# Look up the sum() function to help you do this.
-
-
 def calculate_score(cards):
 
     # Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
@@ -68,7 +54,33 @@ def calculate_score(cards):
         cards.remove(11)
         cards.remove(1)
     return sum(cards)
+
+
+# Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
+user_cards = []
+computer_cards = []
+is_game_over = False
+for _ in range(2):
+    new_card = deal_card()
+    user_cards.append(new_card)
+    computer_cards.append(deal_card())
+
+# Hint 6: Create a function called calculate_score() that takes a List of cards as input
+# and returns the score.
+# Look up the sum() function to help you do this.
+
+
 # Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
+
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+
+print(f" Your cards: {user_cards}, current score: {user_score}")
+print(
+    f" Computer's first cards: {computer_cards[0]}")
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
 
 # Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
 
