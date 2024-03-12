b758cd7cb08f9e961331b672ee7905b85aa5266c
Patt Sookmark <pijiya@sookmark.com>
2024-03-12 15:23:28 -0600

    Add food function and new scoreboard

--- snake-game/main.py
+++ snake-game/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 import time
 screen = Screen()
@@ -9,16 +11,26 @@ screen.title("Super Snake Game")
 screen.tracer(0)
 
 snake = Snake()
-game_is_on = True
+food = Food()
+scoreboard = Scoreboard()
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down,"Down")
 screen.onkey(snake.left,"Left")
 screen.onkey(snake.right,"Right")
+
+game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # Check collision.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+        
 
 screen.exitonclick()
 
