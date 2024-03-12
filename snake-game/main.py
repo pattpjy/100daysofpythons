afc8d301b5d103d08c3321ffff174efcb121eaf7
Patt Sookmark <pijiya@sookmark.com>
2024-03-12 11:36:05 -0600

    Add fn movefoward

--- snake-game/main.py
+++ snake-game/main.py
@@ -1,20 +1,25 @@
 from turtle import Screen, Turtle
-
+import time
 screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor('black')
-screen.title( "Super Snake Game")
-
+screen.title("Super Snake Game")
+screen.tracer(0)
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 segments = []
 
 for position in starting_positions:
     new_segment = Turtle('square')
+    new_segment.penup()
     new_segment.color('white')
     new_segment.goto(position)
     segments.append(new_segment)
 
-    game_is_on = True
-    while game_is_on:
-        for seg in segments:
-            seg.forward(20)
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    for seg in segments:
+        seg.forward(20)
+
