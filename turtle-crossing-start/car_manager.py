efc2597d282ab0fccf056d717232462ce2db80f6
Patt Sookmark <pijiya@sookmark.com>
2024-03-15 11:28:04 -0600

    Add cars and catch collision

--- turtle-crossing-start/car_manager.py
+++ turtle-crossing-start/car_manager.py
@@ -10,13 +10,15 @@ class CarManager(Turtle):
         self.all_cars = []
 
     def create_car(self):
-        new_car = Turtle('square')
-        new_car.shapesize(stretch_wid=1, stretch_len=2)
-        new_car.penup()
-        new_car.color(random.choice(COLORS))
-        random_y = random.randint(-250, 250)
-        new_car.goto(300, random_y)
-        self.all_cars.append(new_car)
+        random_change = random.randint(1,6)
+        if random_change == 1:
+            new_car = Turtle('square')
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
 
     def move_cars(self):
         for car in self.all_cars:
